4cad4437a30ab3484d1ed4be6d7ca0469ad6e02a
audreybichelmeir <bichelmeir@chapman.edu>
2020-03-29 07:52:44 +0000

    updates

--- fileReader.cpp
+++ fileReader.cpp
@@ -36,29 +36,53 @@ string fileReader::readAFile(string file){
 bool fileReader::isBalanced(string& line, int& lineCounter, GenStack<char>& store, fstream& inputStream)
 {
   char x;
-  cout << "c: " << line.size() << endl;
-  cout << "d: " << lineCounter << endl;
-  cout << "e: " << line << endl;
+  // cout << "c: " << line.size() << endl;
+  // cout << "d: " << lineCounter << endl;
+  // cout << "e: " << line << endl;
   // Traversing the Expression
   	for (int i = 0; i < line.size(); ++i)
   	{
-      cout << "wtfff" << endl;
-      cout << "f: " << line[i] << endl;
+      // cout << "wtfff" << endl;
+      // cout << "f: " << line[i] << endl;
       if (line[i] ==  LEFT_ROUND_PARENTHESIS || line[i] ==  LEFT_SQUARE_BRACKETS || line[i] ==  LEFT_CURLY_BRACKETS)
   		{
         cout << "g: " << line[i] << endl;
         // Push the element in the stack
-        cout << "wtf" << endl;
         store.push(line[i]);
   			continue;
   		}
-      else{
-        cout << "doesn't do shit" << endl;
+
+      // IF current current character is not opening
+      // bracket, then it must be closing. So stack
+      // cannot be empty at this point.
+      if (store.isEmpty())
+      return false;
+
+      if(line[i] == RIGHT_ROUND_PARENTHESIS){
+        x = store.peek();
+  			store.pop();
+  			if (x == LEFT_CURLY_BRACKETS || x == LEFT_SQUARE_BRACKETS)
+        //error statement should be here
+  				return false;
       }
 
+      if(line[i] == RIGHT_SQUARE_BRACKETS){
+        x = store.peek();
+  			store.pop();
+  			if (x == LEFT_ROUND_PARENTHESIS || x == LEFT_CURLY_BRACKETS)
+  				return false;
+  			break;
       }
-  	}
 
-  	// Check Empty Stack
-  	return (store.isEmpty());
-  }
+      if(line[i] == RIGHT_CURLY_BRACKETS){
+        x = store.peek();
+        store.pop();
+        if (x == LEFT_ROUND_PARENTHESIS || x == LEFT_SQUARE_BRACKETS)
+  				return false;
+  			break;
+      }
+
+    }
+    // Check Empty Stack
+    return (store.isEmpty());
+}
