25c005e347d0a84558e8498ba26e809faf8f092b
audreybichelmeir <bichelmeir@chapman.edu>
2020-04-03 22:54:18 +0000

    it's working version wait til i fck it u[

--- fileReader.h
+++ fileReader.h
@@ -8,15 +8,18 @@ class fileReader{
 public:
   fileReader();
   ~fileReader();
+  string filePrompt(string error);
   string readAFile(string file);
-  bool isBalanced(string& line, int& lineCounter, GenStack<char>& store, fstream& inputStream);
+  void isBalanced(string& line, int& lineCounter, GenStack<char>& store, fstream& inputStream);
+  //void isBalanced(string& line, int& lineCounter, GenStack<char>& store, fstream& inputStream, char& LEFT_ROUND_PARENTHESIS, char& RIGHT_ROUND_PARENTHESIS, char& LEFT_CURLY_BRACKETS, char& RIGHT_CURLY_BRACKETS, char& LEFT_SQUARE_BRACKETS, char& RIGHT_SQUARE_BRACKETS);
   GenStack<char> store; // Stack to store the lefts as they occur
 
 private:
-  const char LEFT_ROUND_PARENTHESIS = '(';
-  const char RIGHT_ROUND_PARENTHESIS = ')';
-  const char LEFT_CURLY_BRACKETS = '{';
-  const char RIGHT_CURLY_BRACKETS = '}';
-  const char LEFT_SQUARE_BRACKETS = '[';
-  const char RIGHT_SQUARE_BRACKETS = ']';
+  static const char LEFT_ROUND_PARENTHESIS = '(';
+  static const char RIGHT_ROUND_PARENTHESIS = ')';
+  static const char LEFT_CURLY_BRACKETS = '{';
+  static const char RIGHT_CURLY_BRACKETS = '}';
+  static const char LEFT_SQUARE_BRACKETS = '[';
+  static const char RIGHT_SQUARE_BRACKETS = ']';
+
 };
--- main.cpp
+++ main.cpp
@@ -7,13 +7,23 @@ int main(int argc, char **argv)
 {
 
 string file = " ";
+string error = "Looks like the user did not input a file name in the command line... Yikes";
+
   if(argc > 1){
     file = argv[1];
+    fileReader *fr = new fileReader();
+    fr->readAFile(file);
+    delete fr;
   }
+  else{
+    cout << error << endl;
 
-  fileReader *fr = new fileReader();
-  fr->readAFile(file);
-  delete fr;
+    fileReader *fr = new fileReader();
+    fr->filePrompt(error);
+    delete fr;
+
+
+  }
 
   return 0;
 }
